590735771aa66b6543f99117314848a601a82365
yanyu <yanyu1332@yahoo.co.jp>
2022-12-24 00:48:15 +0900

    rawデータを行列データに整形するコードを作成した

--- 03_build/code/create_matrix_of_point_network.R
+++ 03_build/code/create_matrix_of_point_network.R
@@ -7,26 +7,41 @@ point_data_raw <- read.csv("02_raw/data/dataset_point_network.csv")
 ## --------------------------------------------------
 ## master datasetの作成
 #ユニークデータのベクトルを作成する
-unique_point <- unique(point_data_raw$origin)
+unique_point_orgin <- unique(point_data_raw$origin)
+unique_point_exchange <- unique(point_data_raw$exchange)
 
-# masterのdataframeの作成
-master_df <- data.frame(unique_point)
+unique_point <- unique(append(unique_point_orgin,unique_point_exchange))
+unique_point <- unique_point_orgin
+
+# 行列の1行目を意味するdataframeの作成
+df_col_1 <- data.frame(unique_point)
 
 ## --------------------------------------------------
 ## 個々のpointのdataframeを作成
 
 # 個々のdataframeを作成
-df_indvisual <- point_data_raw[point_data_raw$origin == "JALマイレージバンク",] %>%
-  select(-X) # 「X」列以外の列を選択
-
-df_indvisual_remake <- df_indvisual %>%
-  mutate(JALマイレージバンク = 1) %>% #1を要素として持つ新たな列を追加
-  select(-origin) # 「origin」列以外の列を選択 
-
-## masterと個々のポイントのdataframeをouter_join
-master_df <- left_join(master_df,df_indvisual_remake,by = c("unique_point" = "exchange"))
-
+length(unique_point)
+df_temporary <- data.frame(unique_point)
+for (i in 1:length(unique_point)) {
+  df_indvisual <- point_data_raw[point_data_raw$origin == unique_point[i],] %>%
+    select(-X) # 「X」列以外の列を選択
+  df_indvisual_remake <- df_indvisual %>%
+    mutate(hoge = 1) %>% #1を要素として持つ新たな列を追加
+    select(-origin) # 「origin」列以外の列を選択
+  # masterと個々のポイントのdataframeをouter_join
+  df_temporary <- left_join(df_temporary,df_indvisual_remake,by = c("unique_point" = "exchange"))
+}
 ## --------------------------------------------------
 ## detaframの成型
-# NAを0に変換
-master_df[is.na(master_df)] <- 0
+df <- full_join(df_col_1,df_temporary,by = "unique_point") %>%
+  distinct(.keep_all=TRUE)
+df[is.na(df)] <- 0 # NAを0に変換
+
+colnames(df) <- c("point",unique_point) #列名をunique_pointに変更
+
+df <- subset(df, point != "None") %>%
+  select(-None)
+
+## --------------------------------------------------
+# CSV形式でエクスポート
+write.csv(x = df, file = "03_build/output/matrix_of_point.csv")
